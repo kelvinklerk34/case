30e9ead84fbfa3b4ecdfcf9e83265315102bc15f
kelvinklerk <klerkkelvin2@gmail.com>
2023-07-14 08:55:10 -0700

    First Upload

---
+++ main.js
@@ -0,0 +1,21 @@
+$('.btn').click(function(){
+   $('.box').slideToggle(300)
+$('.box').css({
+    'widith': '500px',
+    'height': '500px',
+    'background': 'blue',
+    'transition': '0.3s',
+})
+});
+
+/*
+fadeOut(300)
+fadeIn(300)
+SildeUp(300)
+SildeIn(300)
+slideToggle(300)
+fadeToggle(300)
+hide(300)
+show(300)
+
+*/ 
